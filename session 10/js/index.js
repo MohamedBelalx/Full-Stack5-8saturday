2d8f3ad98e2a5f500159b7c97e0cb62c452784fb
MohamedBelalx <hero.armna@gmail.com>
2020-09-12 20:24:25 +0200

    session 10

---
+++ session 10/js/index.js
@@ -0,0 +1,31 @@
+/* var hours = 50
+var rate = 13.25
+
+console.log(hours * rate) */
+
+var age = 25;
+
+/* if(age < 18)
+{
+    console.log('U Still Too young')
+}else
+{
+    console.log('U are Adult')
+} */
+
+if(age == 20)
+{
+    console.log('Go To Window 1')
+}else if(age == 25)
+{
+    console.log('Go To Window 2')
+}else if(age == 30)
+{
+    console.log('Go To Window 3')
+}else
+{
+    console.log('Go To Window 4')
+}
+
+var x = 25;
+var y = 30;
